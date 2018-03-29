f816eefdd30ea8cce4e803bbd8d858897fd2c085
wilztan <awz77wil@gmail.com>
2018-03-29 22:12:35 +0800

    Succesfully send image

--- src/Components/FormInput.js
+++ src/Components/FormInput.js
@@ -12,7 +12,14 @@ export default class FormInput extends Component{
     const {name,placeholder,type,onChange,value,} = this.props;
     return(
       <div className="form-group">
-        <input className="form-control" value={value} name={name} placeholder={placeholder} type={type} onChange={onChange}/>
+        <input
+          className="form-control"
+          value={value}
+          name={name}
+          placeholder={placeholder}
+          type={type}
+          onChange={onChange}
+        />
       </div>
     );
   }
--- src/Pages/Auth/Admin/FormItem.js
+++ src/Pages/Auth/Admin/FormItem.js
@@ -20,63 +20,69 @@ export default class FormItem extends Component{
       image:'',
     }
     this.handleSubmitForm = this.handleSubmitForm.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleImageChange = this.handleImageChange.bind(this);
     this.handleResult = this.handleResult.bind(this);
   }
   handleResult(result){
-    alert(JSON.stringify(result));
+    if(result.status>0)
+    alert('success');
+    //set function to another page
   }
-  handleSubmitForm(e){
-    e.preventDefault();
-    var data = new FormData();
-    // data.append('image', this.state.product.image);
-    // data.append('name', this.state.product.name);
-    // data.append('stock',this.state.product.stock);
-    // data.append('price',this.state.product.price);
-    // data.append('description',this.state.product.description);
-    alert(JSON.stringify(this.state.product));
+  handleSubmitForm(event) {
+    event.preventDefault();
+    let formData = new FormData();
+    formData.append('image', event.target.myimage.files[0]);
+    formData.append('name', event.target.name.value);
+    formData.append('price', event.target.price.value);
+    formData.append('stock', event.target.stock.value);
+    formData.append('description', event.target.description.value);
     var token = AuthChecker.getUserCookie("PassportToken");
-    var item = JSON.stringify(this.state.product);
-    alert(item);
     fetch(ApiRoute.getProductResourcePath(), {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Authorization':`Bearer ${token}`,
-        // 'Content-Type': 'multipart/form-data',
-      },
-      body: JSON.stringify(this.state.product)
+        method: 'POST',
+        headers: {
+          // 'Content-Type': 'multipart/form-data',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ' + token,
+        },
+         body: formData,
     })
     .then(response => response.json())
     .then(result => this.handleResult(result))
     .catch(e => e);
-  }
-  handleImageChange(e){
-  }
-  handleInputChange(key,e){
-    var state = Object.assign({}, this.state.product);
-    if(key==="image"){
-      state[key] = e.target.files[0];
-    }else{
-      state[key] = e.target.value;
-    }
-    this.setState({product:state});
   }
   render(){
     const {header, method}= this.props;
     return(
       <Panel header={header}>
         <form onSubmit={this.handleSubmitForm} encType="multipart/form-data">
-          <FormInput placeholder="Product Name" type="text" onChange={(e)=>this.handleInputChange("name",e)}/>
+          <FormInput
+            placeholder="Product Name"
+            type="text"
+            name="name"
+          />
 
-          <FormInput placeholder="Product Price" type="number" onChange={(e)=>this.handleInputChange("price",e)}/>
+          <FormInput
+            placeholder="Product Price"
+            type="number"
+            name="price"
+          />
 
-          <FormInput placeholder="Product Stock" type="number" onChange={(e)=>this.handleInputChange("stock",e)}/>
+          <FormInput
+            placeholder="Product Stock"
+            type="number"
+            name="stock"
+          />
 
-          <FormInput placeholder="Product Image" type="file" onChange={(e)=>this.handleInputChange("image",e)}/>
+          <FormInput
+            placeholder="Product Image"
+            type="file"
+            name="myimage"
+          />
 
-          <textarea placeholder="Product Description" onChange={(e)=>this.handleInputChange("description",e)} className="form-control" />
+          <textarea
+            name="description"
+            placeholder="Product Description"
+            className="form-control"
+          />
           <button className="btn btn-submit">Create New Product</button>
         </form>
       </Panel>
